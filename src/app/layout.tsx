71fd0a01a7c2ac9b6dff4c075b927b892a2f1f46
Taiwo Akinnusoye <taiwo@kudan.eu>
2024-05-16 17:09:45 +0100

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { Header } from "./components";
+
+export const metadata: Metadata = {
+  title: "CTRL PRINT",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className="dark-theme">
+      <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Google+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <div className="dots" />
+        <Header />
+        {children}
+        <div className="bottom-gradient" />
+      </body>
+    </html>
+  );
+}
