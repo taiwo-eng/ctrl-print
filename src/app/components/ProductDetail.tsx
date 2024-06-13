53bf95a20801cffff7c99fb094bd4eb7ea834577
Taiwo Akinnusoye <taiwo@kudan.eu>
2024-06-13 13:25:46 +0100

    add item count button

--- src/app/components/ProductDetail.tsx
+++ src/app/components/ProductDetail.tsx
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 
 export function ProductDetail({ params }: { params: { slug: string } }) {
     const [itemCount, setItemCount] = useState(1)
+    const [isSelected, setIsSelected] = useState({
+        s: false,
+        m: false,
+        l: false,
+        xl: false
+    });
     return (
         <div className='product-detail'>
             <div className='details-add-to-cart'>
@@ -18,6 +24,12 @@ export function ProductDetail({ params }: { params: { slug: string } }) {
                     <span aria-disabled={itemCount === 1} onClick={() => setItemCount(itemCount - 1)} className='reduce-count'>-</span>
                     <span>{itemCount}</span>
                     <span aria-disabled={itemCount > 10} onClick={() => setItemCount(itemCount + 1)} className='increase-count'>+</span>
+                </div>
+                <div className='size-select'>
+                    <span className={isSelected.s ? 'selected-S': 'not-selected'} onClick={() => setIsSelected((prevState) => ({...prevState, s: !isSelected.s}))}>S</span>
+                    <span className={isSelected.m ? 'selected-M': 'not-selected'}  onClick={() => setIsSelected((prevState) => ({...prevState, m: !isSelected.m}))}>M</span>
+                    <span className={isSelected.l ? 'selected-L': 'not-selected'}  onClick={() => setIsSelected((prevState) => ({...prevState, l: !isSelected.l}))}>L</span>
+                    <span className={isSelected.xl ? 'selected-XL': 'not-selected'}  onClick={() => setIsSelected((prevState) => ({...prevState, xl: !isSelected.xl}))}>XL</span>
                 </div>
             </div>
             </div>
